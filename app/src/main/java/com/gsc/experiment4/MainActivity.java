31cb4a98341fee5b00163cf31a167c0245e8d76a
suchengao <2316547375@qq.com>
2022-11-30 20:07:20 +0800

    第一次提交

---
+++ app/src/main/java/com/gsc/experiment4/MainActivity.java
@@ -0,0 +1,21 @@
+package com.gsc.experiment4;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        toolbar = findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+    }
+
+}
